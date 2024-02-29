308c6231fb0aa6936e57cabdc85f1437a23e61dc
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2024-02-29 12:01:57 +0100

    [1.1.0] Support Forge Installer 2.2

--- src/main/java/net/kunmc/lab/forgecli/InstallerUtil.java
+++ src/main/java/net/kunmc/lab/forgecli/InstallerUtil.java
@@ -6,8 +6,10 @@ import net.minecraftforge.installer.SimpleInstaller;
 import net.minecraftforge.installer.actions.ActionCanceledException;
 import net.minecraftforge.installer.actions.ProgressCallback;
 import net.minecraftforge.installer.json.Install;
+import net.minecraftforge.installer.json.Util;
 
 import java.io.File;
+import java.lang.reflect.Method;
 
 public class InstallerUtil {
     public static boolean runClientInstall(ProgressCallback monitor, File target, File installerJar) {
@@ -30,6 +32,26 @@ public class InstallerUtil {
                 return new LegacyClientInstall(profile, monitor).run(target, input -> true);
             }
         } catch (ActionCanceledException e) {
+            throw new RuntimeException(e);
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+            return tryNew2_2Install(monitor, target, installerJar);
+        }
+    }
+
+    /**
+     * There is a new 2.2. version of the Installer since November 2023
+     */
+    private static boolean tryNew2_2Install(ProgressCallback monitor, File target, File installerJar) {
+        try {
+            //noinspection JavaReflectionMemberAccess
+            Method method = net.minecraftforge.installer.actions.ClientInstall.class.getDeclaredMethod("run", File.class, File.class);
+            method.setAccessible(true);
+            net.minecraftforge.installer.actions.ClientInstall clientInstall = new net.minecraftforge.installer.actions.ClientInstall(Util.loadInstallProfile(), monitor);
+            return (boolean) method.invoke(clientInstall, target, installerJar);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException("Unknown forge installer version.");
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
